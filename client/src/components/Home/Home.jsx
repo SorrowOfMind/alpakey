50ec10833a16d7b5f134bbde5814eb18e4354a0e
SorrowOfMind <sortoflifecraze@gmail.com>
2022-02-28 22:01:32 +0100

    dashboard

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Typography, Col, Row, Statistic} from 'antd';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Typography.Title level={2}>Dashboard</Typography.Title>
+    <Row>
+      <Col span="6"><Statistic title="Total Adult Females" value="25"/></Col>
+      <Col span="6"><Statistic title="Total Adult Mails" value="8"/></Col>
+      <Col span="6"><Statistic title="Total Baby Females" value="5"/></Col>
+      <Col span="6"><Statistic title="Total Baby Males" value="3"/></Col>
+    </Row>
+    <Row>
+      <Col span="6"><Statistic title="Total Hay" value="200kg"/></Col>
+      <Col span="6"><Statistic title="Total Grains" value="300kg"/></Col>
+      <Col span="6"><Statistic title="Total Pelleted Food" value="200kg"/></Col>
+      <Col span="6"><Statistic title="Total Dry Straw" value="200kg"/></Col>
+    </Row>
+    </>
   )
 }
 
